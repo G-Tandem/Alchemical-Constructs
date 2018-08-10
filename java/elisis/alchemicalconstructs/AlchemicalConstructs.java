baa0ef8cd78d6440a86dee184c1e1e9e01b9b0ac
Jarrad Thomson <gtandemmodding@gmail.com>
2018-08-10 11:21:41 +1000

    Added handlers to main file.

--- java/elisis/alchemicalconstructs/AlchemicalConstructs.java
+++ java/elisis/alchemicalconstructs/AlchemicalConstructs.java
@@ -1,6 +1,11 @@
 package elisis.alchemicalconstructs;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = AlchemicalConstructs.MODID, name = AlchemicalConstructs.NAME, version = AlchemicalConstructs.VERSION)
 public class AlchemicalConstructs {
@@ -8,5 +13,24 @@ public class AlchemicalConstructs {
 	public static final String MODID = "alchemicalconstructs";
 	public static final String NAME = "Alchemical Constructs";
 	public static final String VERSION = "1.7.10a0.1";
-
+	
+	//Not needed *right* now, but will be useful to have in the future. 
+	@Instance
+	public static AlchemicalConstructs instance = new AlchemicalConstructs(); 
+	
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) { 
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postInit(FMLPostInitializationEvent event) {
+		
+	}
+	
 }
