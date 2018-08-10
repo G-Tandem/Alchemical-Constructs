516afe30fc026a062c153f0d1ff28ae35579ce96
Jarrad Thomson <gtandemmodding@gmail.com>
2018-08-10 11:39:05 +1000

    Deal with item files, recipes, etc.

---
+++ java/elisis/alchemicalconstructs/item/ACItems.java
@@ -0,0 +1,15 @@
+package elisis.alchemicalconstructs.item;
+
+import net.minecraft.item.Item;
+
+public class ACItems {
+	
+	public static Item chalk;
+	
+	//Registering
+	public static void init() {
+		
+		
+	}
+
+}
---
+++ java/elisis/alchemicalconstructs/recipe/ACRecipes.java
@@ -0,0 +1,7 @@
+package elisis.alchemicalconstructs.recipe;
+
+public final class ACRecipes {
+	
+	
+
+}
